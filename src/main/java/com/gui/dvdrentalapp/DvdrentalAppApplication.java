28ec7b20437666f3e9d2268d2ba4e1308a3169e7
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2024-04-27 17:49:41 +0200

    Initial commit

---
+++ src/main/java/com/gui/dvdrentalapp/DvdrentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.gui.dvdrentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvdrentalAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gui/dvdrentalapp/DvdrentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.dvdrentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdrentalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
